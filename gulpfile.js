31fedfebeca497c13d67f39f57619a9cc82d2fdf
flocko <flo.kircher@gmail.com>
2015-11-02 22:55:31 +0100

    cleanup comments

--- gulpfile.js
+++ gulpfile.js
@@ -1,37 +1,64 @@
+/**
+ * Available Tasks
+ * gulp       - Starts local dev server and watches for file changes
+ * gulp build - copy src content to app/ for release
+ * gulp test  - html/js/css testing
+ */
+
+
+/**
+ * Plugins
+ * gulp
+ * gulp-autoprefixer
+ * gulp-load-plugins
+ * gulp-plumber
+ * gulp-sass
+ */
 var gulp        = require('gulp');
 var plugins     = require('gulp-load-plugins')();
 var browserSync = require('browser-sync');
 var reload      = browserSync.reload;
 
+/**
+ * Options
+ */
 var src = {
-    scss: 'src/sass/**/*.scss',
-    css:  'src/css',
-    html: 'src/*.html'
+  scss : 'src/sass/**/*.scss',
+  css  : 'src/css',
+  html : 'src/*.html',
+  js   : 'src/js/**/*.js'
 };
 
+/**
+ * Main tasks
+ */
+gulp.task('default', ['dev']);
 
-// start server and watch files
-gulp.task('serve', ['sass'], function() {
 
-    browserSync({
-        server: "./src"
-    });
+/**
+ * Sub tasks
+ */
+// start dev server and watch for file changes
+gulp.task('dev', ['sass'], function() {
+  browserSync({
+    server: "./src"
+  });
 
-    gulp.watch(src.scss, ['sass']);
-    gulp.watch(src.html).on('change', reload);
+  gulp.watch(src.scss, ['sass']);
+  gulp.watch(src.html).on('change', reload);
 });
 
 // compile sass
 gulp.task('sass', function() {
-    return gulp.src(src.scss)
-        .pipe( plugins.plumber() )
-        .pipe( plugins.sass( { outputStyle : 'compressed' }).on('error', plugins.sass.logError))
-        .pipe( plugins.autoprefixer( {
-            browsers: ['last 2 versions'],
-            cascade: false
-        }))        
-        .pipe(gulp.dest(src.css))    
-        .pipe(reload({stream: true}));
+  return gulp.src(src.scss)
+    .pipe( plugins.plumber() )
+    .pipe( plugins.sass( { outputStyle : 'compressed' }).on('error', plugins.sass.logError))
+    .pipe( plugins.autoprefixer( {
+      browsers: ['last 2 versions'],
+        cascade: false
+      }))        
+    .pipe(gulp.dest(src.css))    
+    .pipe(reload({stream: true}));
 });
 
-gulp.task('default', ['serve']);
+// compile js
