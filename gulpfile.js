edc7a673f7dc1cac19a8b25e6ca3e857c48e22c1
flocko <flo.kircher@gmail.com>
2016-02-21 20:20:49 +0100

    lint js gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -36,18 +36,18 @@
  * postcss-scss
  */
 
-var gulp           = require('gulp');
-var plugins        = require('gulp-load-plugins')();
-var browserSync    = require('browser-sync');
+var gulp           = require("gulp");
+var plugins        = require("gulp-load-plugins")();
+var browserSync    = require("browser-sync");
 var reload         = browserSync.reload;
-var htmlreplace    = require('gulp-html-replace');
-var runSequence    = require('run-sequence');
-var mainBowerFiles = require('main-bower-files');
-var del            = require('del');
+var htmlreplace    = require("gulp-html-replace");
+var runSequence    = require("run-sequence");
+var mainBowerFiles = require("main-bower-files");
+var del            = require("del");
 
-var stylelint      = require('stylelint');
-var reporter       = require('postcss-reporter');
-var scss           = require('postcss-scss');
+var stylelint      = require("stylelint");
+var reporter       = require("postcss-reporter");
+var scss           = require("postcss-scss");
 
 /**
  * ---------------------------
@@ -56,42 +56,42 @@ var scss           = require('postcss-scss');
  */
 
 var path = {
-  build : 'app/',
-  dev   : 'src/'
+  build : "app/",
+  dev   : "src/"
 };
 
 var options = {
   sass  : {
-    files : 'sass/**/*.scss',
-    file  : 'sass/style.scss',
-    dest  : 'sass/'
+    files : "sass/**/*.scss",
+    file  : "sass/style.scss",
+    dest  : "sass/"
   },
   css   : {
-    files : 'css/**/*.css',
-    file  : 'style.css',
-    dest  : 'css/'
+    files : "css/**/*.css",
+    file  : "style.css",
+    dest  : "css/"
   },
   html  : {
-    files : '*.html',
-    file  : 'index.html'
+    files : "*.html",
+    file  : "index.html"
   },
   js    : {
-    files : 'js/**/*.js',
-    file  : 'app.js',
-    dest  : 'js/'
+    files : "js/**/*.js",
+    file  : "app.js",
+    dest  : "js/"
   },
   img   : {
-    files : 'img/**.*',
-    dest  : 'img/',
+    files : "img/**.*",
+    dest  : "img/",
   },
   fonts : {
-    files: 'fonts/**/*',
-    dest : 'fonts/'
+    files: "fonts/**/*",
+    dest : "fonts/"
   }
 };
 
 var vendor = {
-  sass : 'sass/vendor'
+  sass : "sass/vendor"
 };
 
 /**
@@ -100,15 +100,15 @@ var vendor = {
  * ---------------------------
  */
 
-gulp.task('default', ['dev']);
+gulp.task("default", ["dev"]);
 
 // run-sequence until gulp 4.0
-gulp.task('build', function(done) {
-  runSequence('build:clean', 'sass', ['copy:style','copy:html', 'copy:img', 'copy:fonts', 'minify:js'], 'html:replace', done);
+gulp.task("build", function(done) {
+  runSequence("build:clean", "sass", ["copy:style","copy:html", "copy:img", "copy:fonts", "minify:js"], "html:replace", done);
 });
 
-gulp.task('exec', function(done) {
-  runSequence('bower:install', 'bower:copy', done);
+gulp.task("exec", function(done) {
+  runSequence("bower:install", "bower:copy", done);
 });
 
 /**
@@ -118,23 +118,23 @@ gulp.task('exec', function(done) {
  */
 
 // start dev server and watch for file changes
-gulp.task('dev', ['sass'], function() {
+gulp.task("dev", ["sass"], function() {
   browserSync({
     server: "./src"
   });
 
-  gulp.watch(path.dev + options.sass.files, ['sass']);
-  gulp.watch(path.dev + options.html.files).on('change', reload);
-  gulp.watch(path.dev + options.js.files).on('change', reload);
+  gulp.watch(path.dev + options.sass.files, ["sass"]);
+  gulp.watch(path.dev + options.html.files).on("change", reload);
+  gulp.watch(path.dev + options.js.files).on("change", reload);
 });
 
 // compile sass
-gulp.task('sass', function() {
+gulp.task("sass", function() {
   gulp.src( path.dev + options.sass.files )
   .pipe( plugins.plumber() )
-  .pipe( plugins.sass( { outputStyle : 'nested' }).on('error', plugins.sass.logError))
+  .pipe( plugins.sass( { outputStyle : "nested" }).on("error", plugins.sass.logError))
   .pipe( plugins.autoprefixer( {
-    browsers: ['last 2 versions'],
+    browsers: ["last 2 versions"],
       cascade: false
     }))
   .pipe(gulp.dest( path.dev + options.css.dest ))
@@ -149,7 +149,7 @@ gulp.task('sass', function() {
  **/
 
 // concat all js files for build
-gulp.task('minify:js', function() {
+gulp.task("minify:js", function() {
   return gulp.src(path.dev + options.js.files)
   .pipe( plugins.plumber() )
   .pipe( plugins.concat( options.js.file ) )
@@ -158,18 +158,18 @@ gulp.task('minify:js', function() {
 });
 
 // replace links for build
-gulp.task('html:replace', function() {
+gulp.task("html:replace", function() {
   gulp.src( path.build + options.html.files )
   .pipe( plugins.plumber() )
   .pipe( htmlreplace({
-    'css' : options.css.dest + options.css.file,
-    'js'  : options.js.dest + options.js.file
+    "css" : options.css.dest + options.css.file,
+    "js"  : options.js.dest + options.js.file
   }))
   .pipe(gulp.dest( path.build ));
 });
 
 // copy images and perform minification
-gulp.task('copy:img', function() {
+gulp.task("copy:img", function() {
   return gulp.src( path.dev + options.img.files )
   .pipe( plugins.plumber() )
   .pipe( plugins.imagemin({
@@ -179,26 +179,26 @@ gulp.task('copy:img', function() {
 });
 
 // copy files to app folder
-gulp.task('copy:style', function() {
+gulp.task("copy:style", function() {
   return gulp.src( path.dev + options.css.dest + options.css.file )
   .pipe( gulp.dest( path.build + options.css.dest ) );
 });
 
 // copy html files to app folder
-gulp.task('copy:html', function() {
+gulp.task("copy:html", function() {
   return gulp.src( path.dev + options.html.files )
   .pipe( gulp.dest( path.build ) );
 });
 
 // copy font files to app folder
-gulp.task('copy:fonts', function() {
+gulp.task("copy:fonts", function() {
   return gulp.src( path.dev + options.fonts.files )
   .pipe( gulp.dest( path.build + options.fonts.dest ) );
 });
 
 // delete app folder
-gulp.task('build:clean', function() {
-  return del('app/');
+gulp.task("build:clean", function() {
+  return del("app/");
 });
 
 /**
@@ -208,17 +208,17 @@ gulp.task('build:clean', function() {
  **/
 
 // install bower components
-gulp.task('bower:install', function() {
-  return plugins.bower({ cmd: 'install'});
+gulp.task("bower:install", function() {
+  return plugins.bower({ cmd: "install"});
 });
 
 // copy bower files
-gulp.task('bower:copy', function() {
-  var jsFilter   = plugins.filter('**/*.js', {restore:true});
-  var scssFilter = plugins.filter('**/*.scss', {restore:true});
-  var fontFilter = plugins.filter(['**/*.eot', '**/*.woff', '**/*.svg', '**/*.ttf', '**/*.woff2'], {restore:true});
+gulp.task("bower:copy", function() {
+  var jsFilter   = plugins.filter("**/*.js", {restore:true});
+  var scssFilter = plugins.filter("**/*.scss", {restore:true});
+  var fontFilter = plugins.filter(["**/*.eot", "**/*.woff", "**/*.svg", "**/*.ttf", "**/*.woff2"], {restore:true});
 
-  return gulp.src( mainBowerFiles({includeDev:true}), { base: './bower_components' } )
+  return gulp.src( mainBowerFiles({includeDev:true}), { base: "./bower_components" } )
   .pipe( plugins.plumber() )
   .pipe( fontFilter )
   .pipe( plugins.flatten() )
@@ -241,8 +241,8 @@ gulp.task('bower:copy', function() {
  **/
 
 // stylelint
-gulp.task('lint:scss', function() {
-  return gulp.src([path.dev + options.sass.files, '!src/sass/vendor{,/**}'])
+gulp.task("lint:scss", function() {
+  return gulp.src([path.dev + options.sass.files, "!src/sass/vendor{,/**}"])
   .pipe(plugins.postcss([
     stylelint(),
     reporter({clearMessages: true})
